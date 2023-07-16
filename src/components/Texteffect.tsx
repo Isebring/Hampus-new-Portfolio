7978dc8c374353c95edebd7f4feccbbeb5280e57
Hampus Isebring <hampus.isebring@gmail.com>
2023-07-17 00:07:03 +0200

    updated welcome text

--- src/components/Texteffect.tsx
+++ src/components/Texteffect.tsx
@@ -1,27 +1,25 @@
-import { Box, Flex, Text } from '@mantine/core';
+import { Box, Text } from '@mantine/core';
 import React, { useState } from 'react';
 import TextTransition, { presets } from 'react-text-transition';
 
 function Texteffect() {
-  const welcomeMessage = ['Hello World', 'Welcome to..', 'my portfolio!'];
+  const welcomeMessage = ['Hello World 🌍', 'Welcome to..', 'my portfolio👋'];
 
   const [index, setIndex] = useState(0);
 
   React.useEffect(() => {
-    const intervalId = setInterval(() => setIndex((index) => index + 1), 2000);
+    const intervalId = setInterval(() => setIndex((index) => index + 1), 3000);
     return () => clearTimeout(intervalId);
   }, []);
 
   return (
-    <Flex justify="center">
-      <Box>
-        <Text fw={500} ta="center" mb="sm" size="xl">
-          <TextTransition springConfig={presets.slow}>
-            {welcomeMessage[index % welcomeMessage.length]}
-          </TextTransition>
-        </Text>
-      </Box>
-    </Flex>
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Text fw={300} ta="center" mb="sm" size={23}>
+        <TextTransition springConfig={presets.slow}>
+          {welcomeMessage[index % welcomeMessage.length]}
+        </TextTransition>
+      </Text>
+    </Box>
   );
 }
 
